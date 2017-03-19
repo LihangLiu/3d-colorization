88a33e186725ad51c30d72b383357d782291550c
Lihang Liu <leonliu@cs.utexas.edu>
2017-03-18 22:16:09 -0500

    epoch 29. cgan+mask.

---
+++ src/cgan-mask/config.py
@@ -0,0 +1,10 @@
+
+# dataset_path = "./data/ShapeNetCore.v2/02958343/*/models/*npy"
+dataset_path = "../../data/npy_list.txt"
+
+version = 29
+ITER_MAX = 2001
+loss_csv = "../../outputs/losses/loss_%d.csv"%(version)
+vox_prefix = "../../outputs/voxels/epoch%d_"%(version)
+
+
