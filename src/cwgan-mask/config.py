7bc53f55620bfd1e20658fd7478e3d53768ca95a
Lihang Liu <leonliu@cs.utexas.edu>
2017-03-22 20:01:32 -0500

    epoch 41. cwgan+mask, deeperG. noise_x noise stddev=.1

---
+++ src/cwgan-mask/config.py
@@ -0,0 +1,8 @@
+dataset_path = "../../data/npy_list.txt"
+
+version = 41
+ITER_MAX = 2001
+loss_csv = "../../outputs/losses/loss_%d.csv"%(version)
+vox_prefix = "../../outputs/voxels/epoch%d_"%(version)
+param_prefix = "../../outputs/params/params%d_"%(version)
+
