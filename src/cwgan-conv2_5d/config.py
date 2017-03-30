dbd1aec97668108c2fc51ed7867bde3f02682124
Lihang Liu <leonliu@cs.utexas.edu>
2017-03-30 15:09:59 -0500

    epoch 63&64. epoch 63: 60: cwgan + conv2.5d             D=0.02, max(1-d,0)             D: h1-h4, xyz project             G: h1-h5, xyz project             G: dh1-dh5, xyz project
    epoch 64:
            63: cwgan + conv2.5d
                D=0.02, max(1-alpha*d,0)
                D: h1-h4, xyz project
                G: h1-h5, xyz project
                G: dh1-dh5, xyz project

--- src/cwgan-conv2_5d/config.py
+++ src/cwgan-conv2_5d/config.py
@@ -1,6 +1,6 @@
 dataset_path = "../../data/npy_list.txt"
 
-version = 62
+version = 64
 ITER_MAX = 2001
 loss_csv = "../../outputs/losses/loss_%d.csv"%(version)
 vox_prefix = "../../outputs/voxels/epoch%d_"%(version)
