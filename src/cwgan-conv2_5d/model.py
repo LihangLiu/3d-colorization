aa6701b3bab4ed06456870d2d55ef6be80ba4979
Lihang Liu <leonliu@cs.utexas.edu>
2017-04-03 10:43:14 -0500

    epoch 1004. cwgan + conv2_5d             D=0.02, exp(-alpha*d^2), init alpha=0.1

--- src/cwgan-conv2_5d/model.py
+++ src/cwgan-conv2_5d/model.py
@@ -38,8 +38,8 @@ def flt2_5d_to_3d(filter, axis, depth):
 
 	# list of depth of filiter_3d, [-D/2, D/2]
 	D = 0.02		# factor
-	#with tf.variable_scope(ALPHA_NAME):
-	alpha = 300*bias_variable([])	# init to be 3.0, D clip considerred
+	with tf.variable_scope(ALPHA_NAME):
+		alpha = bias_variable([])	# init to be 0.1
 	di_s = (np.arange(0.0,depth,1.0)/(depth-1)-0.5)*D #(-0.01,0.01)
 
 	v,d = tf.split(filter, num_or_size_splits=2, axis=axis)
