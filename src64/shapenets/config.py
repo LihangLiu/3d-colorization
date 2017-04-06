ae87fb368c3e4048c3e0faf92c8bc64ac4c62cc8
Lihang Liu <leonliu@cs.utexas.edu>
2017-04-06 17:53:31 -0500

    epoch 2001. shapenets init

---
+++ src64/shapenets/config.py
@@ -0,0 +1,9 @@
+train_dataset_path = "../../data/npy_list.all.64.points.txt.train"
+test_dataset_path = "../../data/npy_list.all.64.points.txt.test"
+
+version = 2001
+ITER_MAX = 501
+loss_csv = "../../outputs/losses/loss_%d.csv"%(version)
+vox_prefix = "../../outputs/voxels/epoch%d_"%(version)
+param_prefix = "../../outputs/params/params%d_"%(version)
+
