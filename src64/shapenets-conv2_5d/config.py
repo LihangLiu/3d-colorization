496bb9b99941dc0f377f9d45a0139158e5a693d0
Lihang Liu <leonliu@cs.utexas.edu>
2017-04-06 20:22:30 -0500

    epoch 2002. shapenets + conv2_5d. D=0.02, exp(-alpha*(d)^2), init alpha=0.1

---
+++ src64/shapenets-conv2_5d/config.py
@@ -0,0 +1,11 @@
+train_dataset_path = "../../data/npy_list.all.64.points.txt.train"
+test_dataset_path = "../../data/npy_list.all.64.points.txt.test"
+
+version = 2002
+ITER_MAX = 501
+loss_csv = "../../outputs/losses/loss_%d.csv"%(version)
+vox_prefix = "../../outputs/voxels/epoch%d_"%(version)
+param_prefix = "../../outputs/params/params%d_"%(version)
+log_txt = "../../outputs/log/log_%d.txt"%(version)
+
+
